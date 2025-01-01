274c64eba51a822d5928008c818945e98ba0d3e3
bayareadevlead@gmail.com <bayareadevlead@gmail.com>
2025-01-01 11:44:01 -0800

    spring init

---
+++ src/test/java/com/tunester/services/artisitprofile/ArtisitprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunester.services.artisitprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisitprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
